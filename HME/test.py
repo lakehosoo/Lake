7eab6e1ff83c3ee9a3fd5a20fe7d79c04765ceec
lakehosoo <38056476+lakehosoo@users.noreply.github.com>
2020-03-30 23:24:50 +0900

    Update test.py

--- HME/test.py
+++ HME/test.py
@@ -5,13 +5,13 @@ import seaborn as sns
 
 #data_value = pd.read_excel('HME_Data_V2.xlsx', sheet_name='Rearrange', index_col=0, header=None, skiprows=2).transpose()
 #data_index = pd.read_excel('HME_Data_V2.xlsx', sheet_name='Rearrange', index_col=0, header=None,nrows=2, dtype=object).transpose()
-data_value = pd.read_csv('data', index_col=0, header=None, skiprows=2).transpose()
-data_index = pd.read_csv('data', index_col=0, header=None,nrows=2, dtype=object).transpose()
+data_value = pd.read_csv('HME/data.csv', index_col=0, header=None, skiprows=2).transpose()
+data_index = pd.read_csv('HME/data.csv', index_col=0, header=None,nrows=2, dtype=object).transpose()
 data_index.columns=["Name","Grade"]
 
 # Grade Information
 for i in range(data_index.shape[0]):
-    if data_index.iloc[i,1]==0:
+    if data_index.iloc[i,1]=='0':
         data_index.iloc[i,1]=data_index.iloc[i-1,1]
 
 data = pd.concat([data_index,data_value],axis=1)
@@ -21,7 +21,6 @@ data.info()
 #data.hist(figsize=(20, 15))
 #pd.scatter_matrix(data, diagonal='kde', color='b', alpha=0.3, figsize=(20, 15))
 data['Grade'].value_counts()
-data['Grade'].hist()
 plt.scatter(data['Prop1'],data['Prop15'])
 
 '''
@@ -37,6 +36,7 @@ for i in range(18):
         data = data.drop(columns='Prop'+f'{i+1}')
 '''
 # New Output
+data['Prop00']=0
 name = []
 for i in range(data_index.shape[0]):
     if (name != data['Name'][i+1]):
@@ -45,32 +45,33 @@ for i in range(data_index.shape[0]):
             Prop_base = data['Prop15'].iloc[i]
         elif data['Prop17'][i+1]:
             Prop_base = data['Prop17'].iloc[i]
-        data['Prop_New'].iloc[i] = 0
+        data['Prop00'].iloc[i] = 0
     else:
         if data['Prop15'].iloc[i] > 0:
             if data['Prop15'].iloc[i] > Prop_base:
-                data['Prop_New'].iloc[i] = 1
+                data['Prop00'].iloc[i] = 1
             else:
-                data['Prop_New'].iloc[i] = -1
+                data['Prop00'].iloc[i] = -1
         elif data['Prop17'][i+1] > 0:
             if data['Prop17'][i+1] > Prop_base:
-                data['Prop_New'].iloc[i] = 1
+                data['Prop00'].iloc[i] = 1
             else:
-                data['Prop_New'].iloc[i] = -1
+                data['Prop00'].iloc[i] = -1
         else:
-            data['Prop_New'].iloc[i] = np.nan
+            data['Prop00'].iloc[i] = np.nan
 
 #data.apply(lambda x: x['Grade']=='REF1', axis=1)
 #data5 = data[data['Grade']=='REF5']
 #data[['Name','Prop15','Prop17','Prop_New']][0:20]
-i = data[data['Prop_New'].isnull() == True].index
+i = data[data['Prop00'].isnull() == True].index
 data_drop = data.drop(i)
 
 data_drop.shape
 sns.set(style="darkgrid")
-ax = sns.countplot(x="Prop_New", hue="Grade", data=data_drop)
-
-sns.pairplot(data_drop, hue="Prop_New")
+sns.countplot(x="Prop00", hue="Grade", data=data_drop)
+sns.catplot(x='Name',y='Prop15',hue='Grade',data=data)
+sns.catplot(x='Name',y='Prop17',hue='Grade',data=data)
+#sns.pairplot(data_drop, hue="Prop_New")
 
 #data[data['Grade']=='REF5'],
 #data.hist(figsize=(20, 15))
